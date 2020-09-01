18f34751877543f05da17c621ac2ae1db8991a3a
Tobias Gerken <tobias.gerken@live.de>
2020-09-01 16:03:06 -0400

    model update

--- Photosynthesis.py
+++ Photosynthesis.py
@@ -4,8 +4,8 @@ from random import uniform
 def model(T=15,LAI=8,PAR = 500, CO2 = 400): 
     
     # add random noise to inputs
-    T = T+uniform(-1, .2)
-    LAI = T+uniform(-.5, +.5)
+    T = T+uniform(-.1, .1)
+    LAI = LAI+uniform(-.5, +.5)
     PAR = PAR+uniform(-25, +25)
     CO2 = CO2+uniform(5, +5)
     # empiral photosynthesis model simplified from
@@ -16,9 +16,9 @@ def model(T=15,LAI=8,PAR = 500, CO2 = 400):
     
     
     # parameters of model
-    K = 0.18    # no uit
-    B = 600      # µmol m–2 s–1 
-    Pmax  = 9     # 9 µmol m–2 s–1 
+    K = 0.18    # no unit
+    B = 300      # µmol m–2 s–1 
+    Pmax  = 9     # µmol m–2 s–1 
     CO2ref = 400 # ppm
     gamma = 50   # ppm
     KCO2 = 500   # ppm
@@ -29,11 +29,12 @@ def model(T=15,LAI=8,PAR = 500, CO2 = 400):
     cTh= .4
     cTc = .25 
     Tmin = 0
-    Tmax = 45
+    Tmax = 40
+
 
     # minimum and maximum temperature relationshio
-    fTh = 1+np.exp(cTh*(T-Tmax))
-    fTc = 1+np.exp(cTh*(Tmin-T))
+    fTh = 1-np.exp(cTh*(T-Tmax))
+    fTc = 1-np.exp(cTc*(Tmin-T))
     f_T = fTc*fTh 
 
     f_LAI= 1 / K * (1  - np.exp(-K * LAI))
@@ -41,5 +42,7 @@ def model(T=15,LAI=8,PAR = 500, CO2 = 400):
     f_CO2 = (CO2 - gamma) / (CO2 + gamma + KCO2) * (CO2ref + gamma + KCO2 ) / (CO2ref - gamma)
 
     P = f_LAI * f_PAR *  f_T * f_CO2
-  
-    return round(P,2)
+    P = max(0,P)
+
+    return  round(P,2)
+
